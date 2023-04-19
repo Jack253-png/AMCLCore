b2e97b25d92c61dee63754476e9c38af3e793ef5
Jack253-png <guoxiuchen20170402@163.com>
2023-04-19 18:54:56 +0800

    updated on 2022.4.19

--- core/src/main/java/com/mcreater/amclcore/MetaData.java
+++ core/src/main/java/com/mcreater/amclcore/MetaData.java
@@ -5,11 +5,13 @@ import lombok.Getter;
 public class MetaData {
     /**
      * AMCL/AMCLCore azure application id
+     * AMCL/AMCLCore azure 应用ID
      */
     @Getter
     public static final String oauthDefaultClientId = "1a969022-f24f-4492-a91c-6f4a6fcb373c";
     /**
      * client id override, using command line {@code -Damclcore.oauth.clientid.override=YOUR_CLIENTID}
+     * 客户端ID 覆盖, 使用命令行 {@code -Damclcore.oauth.clientid.override=你的客户端ID}
      */
     @Getter
     public static final String oauthClientIdOverridePropertyName = "amclcore.oauth.clientid.override";
